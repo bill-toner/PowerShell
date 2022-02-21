c4407a57f2fe4bcedacc1d409cdc1bab7e79cfab
Bill Toner <toner.bill@gmail.com>
2022-02-21 06:47:14 -0800

    Add files via upload

---
+++ Server-Monitoring.ps1
@@ -0,0 +1,15 @@
+﻿cls
+Write-Host "Server Monitoring on Many Machines" -BackgroundColor DarkGray
+$Servers = Import-Csv C:\Users\Administrator.ADATUM\Documents\servers.csv
+Clear-Content C:\Users\Administrator.ADATUM\Documents\Services.csv
+
+ForEach ($item in $servers)
+{
+    Write-Host "Connecting to the Server" $item.ServerName -BackgroundColor Black -ForegroundColor Yellow
+    $Result = Get-Service -ComputerName $item.Servername | ? {$_.DisplayName -like "Remote*"} 
+    $Result | Ft Status, Name, Displayname, MachineName
+    $Result | Select Status, Name, Displayname, MachineName | 
+        Export-Csv C:\Users\Administrator.ADATUM\Documents\Services.csv -NoTypeInformation -Append
+    Clear-Variable Result
+}
+
---
+++ Service-Restart.ps1
@@ -0,0 +1,17 @@
+﻿cls
+Write-Host "Restart Service on Many Machines" -BackgroundColor DarkGray
+$Servers = Import-Csv C:\Users\Administrator.ADATUM\Documents\servers.csv
+
+Foreach ($server in $servers)
+{
+    Write-Host "Connecting to the Server" $server.ServerName -BackgroundColor Black -ForegroundColor Yellow
+    Invoke-Command -ComputerName $server.ServerName -ScriptBlock { Restart-Service -Name bits }
+    if ($? -eq "True")
+    {
+        Write-Host "The service got restarted successfully" -BackgroundColor DarkGray
+    }
+    else
+    {
+        Write-Host "The Service restart failed" -BackgroundColor DarkRed
+    }
+}
