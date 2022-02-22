90e89fc4a8e3e888d5f75c9c2cc7ff305238ca34
Bill Toner <toner.bill@gmail.com>
2022-02-22 05:08:58 -0800

    Update Server-Monitoring.ps1

--- Server-Monitoring.ps1
+++ Server-Monitoring.ps1
@@ -2,6 +2,13 @@ cls
 Write-Host "Server Monitoring on Many Machines" -BackgroundColor DarkGray
 $Servers = Import-Csv C:\Users\Administrator.ADATUM\Documents\servers.csv
 Clear-Content C:\Users\Administrator.ADATUM\Documents\Services.csv
+$Success = 0
+$Failure = 0
+$Date = Get-Date -Format dd_MM_yyyy_HH_mm
+$SuccessFilename = "Success_Machines_$date.txt"
+$FailureFilename = "Failure_Machines_$date.txt"
+Add-Content -Path C:\Users\Administrator.ADATUM\Documents\$SuccessFilename -Value (Get-Date)
+Add-Content -Path C:\Users\Administrator.ADATUM\Documents\$FailureFilename -Value (Get-Date)
 
 ForEach ($item in $servers)
 {
@@ -12,11 +19,19 @@ ForEach ($item in $servers)
         $Result | Ft Status, Name, Displayname, MachineName
         $Result | Select Status, Name, Displayname, MachineName | 
             Export-Csv C:\Users\Administrator.ADATUM\Documents\Services.csv -NoTypeInformation -Append
+        Add-Content -Path C:\Users\Administrator.ADATUM\Documents\$SuccessFileName -Value $item.ServerName
         Clear-Variable Result
+        $Result = $null
+        $Success++
     }
     Catch
     {
         Write-Host "Unable to connect to the Server" $item.ServerName -BackgroundColor DarkRed
+        Add-Content -Path C:\Users\Administrator.ADATUM\Documents\$FailureFileName -Value $item.ServerName
+        $Failure++
     }
 }
+"`n"
+Write-Host "Success = $Success" -BackgroundColor DarkGreen
+Write-Host "Failure = $Failure" -BackgroundColor DarkRed
 
